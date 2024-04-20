f9a8eb1991aeefa4d469050867430b49925054b8
AthallaITS <5027231018@student.its.ac.id>
2024-04-20 21:06:22 +0700

    refactor(soal_3): change monitorProcessto logic to a function

--- soal_3/admin.c
+++ soal_3/admin.c
@@ -11,14 +11,63 @@
 
 #define MAX_LINE_LENGTH 256
 
-int main(int argc, char* argv[]) {
-  FILE* ps_output;
+void monitorProcess(int monitorMode, const char* user) {
   char cmd[MAX_LINE_LENGTH];
+  FILE* ps_output;
   char line[MAX_LINE_LENGTH];
   char comm[MAX_LINE_LENGTH];
-  char process_name[MAX_LINE_LENGTH];
   char log_filename[MAX_LINE_LENGTH];
   FILE* log_file;
+  int pid_process;
+
+  // Mengambil pid dan nama proses
+  snprintf(cmd, sizeof(cmd), "ps -u %s -o pid,comm | awk 'NR>1 {print $1, $2}'", user);
+  ps_output = popen(cmd, "r");
+  if (ps_output == NULL) {
+    perror("Failed to open pipe");
+    exit(EXIT_FAILURE);
+    }
+
+  // Buka log file
+  snprintf(log_filename, sizeof(log_filename), "%s.log", user);
+  log_file = fopen(log_filename, "a");
+  if (log_file == NULL) {
+    perror("Failed to open log file");
+    exit(EXIT_FAILURE);
+    }
+
+  // Baca output dari ps per baris
+  while (fgets(line, sizeof(line), ps_output) != NULL) {
+    // Parsing baris untuk mendapatkan PID dan nama proses
+    if (sscanf(line, "%d %s", &pid_process, comm) != 2) {
+      fprintf(stderr, "Failed to parse line: %s", line);
+      continue;
+      }
+
+    // Mendapatkan waktu saat ini
+    time_t now;
+    struct tm* local_time;
+    char timestamp[30];
+    time(&now);
+    local_time = localtime(&now);
+    strftime(timestamp, sizeof(timestamp), "[%d:%m:%Y]-[%H:%M:%S]", local_time);
+
+    // Format data dan simpan ke log file
+    if (monitorMode == 1) {
+      fprintf(log_file, "%s-%d-%s_JALAN\n", timestamp, pid_process, comm);
+      }
+    else if (monitorMode == 2) {
+      fprintf(log_file, "%s-%d-%s_GAGAL\n", timestamp, pid_process, comm);
+      }
+    }
+
+  // Tutup pipe dan log file
+  pclose(ps_output);
+  fclose(log_file);
+  }
+
+int main(int argc, char* argv[]) {
+  char process_name[MAX_LINE_LENGTH];
   pid_t pid, sid;
 
   if (argc < 2) {
@@ -28,7 +77,6 @@ int main(int argc, char* argv[]) {
 
   int monitorMode = 0;
   char* user = argv[1];
-  int pid_process;
 
   if (argc == 2) {
     char* args[] = { "ps", "-u", user, NULL };
@@ -79,8 +127,6 @@ int main(int argc, char* argv[]) {
     return 1;
     }
 
-
-
   if (argc == 3 && strcmp(argv[1], "-c") == 0) {
     fprintf(stderr, "Memulai proses gagalkan monitoring user %s", argv[2]);
     monitorMode = 2;
@@ -97,87 +143,8 @@ int main(int argc, char* argv[]) {
 
 
   while (1) {
-
-    if (monitorMode == 1) {
-      // Mengambil pid dan nama proses
-      snprintf(cmd, sizeof(cmd), "ps -u %s -o pid,comm | awk 'NR>1 {print $1, $2}'", user);
-      ps_output = popen(cmd, "r");
-      if (ps_output == NULL) {
-        perror("Failed to open pipe");
-        exit(EXIT_FAILURE);
-        }
-
-      // Buka log file
-      snprintf(log_filename, sizeof(log_filename), "%s.log", user);
-      log_file = fopen(log_filename, "a");
-      if (log_file == NULL) {
-        perror("Failed to open log file");
-        exit(EXIT_FAILURE);
-        }
-
-      // Baca output dari ps per baris
-      while (fgets(line, sizeof(line), ps_output) != NULL) {
-        // Parsing baris untuk mendapatkan PID dan nama proses
-        if (sscanf(line, "%d %s", &pid_process, comm) != 2) {
-          fprintf(stderr, "Failed to parse line: %s", line);
-          continue;
-          }
-
-        // Mendapatkan waktu saat ini
-        time_t now;
-        struct tm* local_time;
-        char timestamp[30];
-        time(&now);
-        local_time = localtime(&now);
-        strftime(timestamp, sizeof(timestamp), "[%d:%m:%Y]-[%H:%M:%S]", local_time);
-        // Format data dan simpan ke log file
-        fprintf(log_file, "%s-%d-%s_JALAN\n", timestamp, pid_process, comm);
-        }
-      // Tutup pipe dan log file
-      pclose(ps_output);
-      fclose(log_file);
-      }
-
-    if (monitorMode == 2) {
-      // Mengambil pid dan nama proses
-      snprintf(cmd, sizeof(cmd), "ps -u %s -o pid,comm | awk 'NR>1 {print $1, $2}'", user);
-      ps_output = popen(cmd, "r");
-      if (ps_output == NULL) {
-        perror("Failed to open pipe");
-        exit(EXIT_FAILURE);
-        }
-
-      // Buka log file
-      snprintf(log_filename, sizeof(log_filename), "%s.log", user);
-      log_file = fopen(log_filename, "a");
-      if (log_file == NULL) {
-        perror("Failed to open log file");
-        exit(EXIT_FAILURE);
-        }
-
-      // Baca output dari ps per baris
-      while (fgets(line, sizeof(line), ps_output) != NULL) {
-        // Parsing baris untuk mendapatkan PID dan nama proses
-        if (sscanf(line, "%d %s", &pid_process, comm) != 2) {
-          fprintf(stderr, "Failed to parse line: %s", line);
-          continue;
-          }
-
-        // Mendapatkan waktu saat ini
-        time_t now;
-        struct tm* local_time;
-        char timestamp[30];
-        time(&now);
-        local_time = localtime(&now);
-        strftime(timestamp, sizeof(timestamp), "[%d:%m:%Y]-[%H:%M:%S]", local_time);
-        // Format data dan simpan ke log file
-        fprintf(log_file, "%s-%d-%s_GAGAL\n", timestamp, pid_process, comm);
-        }
-
-      // Tutup pipe dan log file
-      pclose(ps_output);
-      fclose(log_file);
-
+    if (monitorMode == 1 || monitorMode == 2) {
+      monitorProcess(monitorMode, user);
       }
 
     sleep(1);
